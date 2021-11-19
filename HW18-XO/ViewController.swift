93706eb51c262d42adb71bba86c5b6f899f71c4b
DmitryP0l <dmitrypoletaeu@gmail.com>
2021-11-19 21:36:21 +0300

    Initial Commit

---
+++ HW18-XO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW18-XO
+//
+//  Created by lion on 19.11.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
