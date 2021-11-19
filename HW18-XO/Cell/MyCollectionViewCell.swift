5f4ec4ea3f1f27ceef93e8d2fb104507ba1a8abb
DmitryP0l <dmitrypoletaeu@gmail.com>
2021-11-19 23:24:26 +0300

    commit

---
+++ HW18-XO/Cell/MyCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MyCollectionViewCell.swift
+//  HW18-XO
+//
+//  Created by lion on 19.11.21.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+   
+    @IBOutlet weak var symbolLabel: UILabel!
+    
+    static let identifier = "MyCollectionViewCell"
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- HW18-XO/ViewController.swift
+++ HW18-XO/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let SymbolArray: [String] = ["X", "O"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName:"MyCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
     }
 
 
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MyCollectionViewCell", for: indexPath) as! MyCollectionViewCell
+        return cell
+        
+    }
+    // pressed. надо добавить в выбранную ячейку рандом из SymbolArray
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+       // collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = CGFloat(collectionView.frame.width / 3.2)
+            return CGSize(width: size, height: size)
+    }
 }
 
